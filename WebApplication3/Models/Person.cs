ba4667ae12799a368a57b7402563facb795eab1e
JiunJwu <pd4058@yahoo.com.tw>
2020-10-21 23:32:28 +0800

    欄位值最高到99 語系資源

--- WebApplication3/Models/Person.cs
+++ WebApplication3/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,10 @@ namespace WebApplication3.Models
     public class Person
     {
         public int Id { get; set; }
+        [Required(ErrorMessageResourceName = "Person_Name_Required",ErrorMessageResourceType =typeof(Resource1))]
         public string Name { get; set; }
+        [Required(ErrorMessage = "請輸入年紀")]
+        [Range(18,99,ErrorMessage ="須為18~99歲")]
         public int Age { get; set; }
     }
 }
