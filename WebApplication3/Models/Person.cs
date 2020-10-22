bf1cc256c1e06d7e1d705e8dbe9cd7d614e00648
JiunJwu <pd4058@yahoo.com.tw>
2020-10-22 23:01:52 +0800

    從model加入[DisplayName]與示範單一頁面套用不同欄位名稱

--- WebApplication3/Models/Person.cs
+++ WebApplication3/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,10 @@ namespace WebApplication3.Models
     public class Person
     {
         public int Id { get; set; }
+        [DisplayName("姓名")]
         [Required(ErrorMessageResourceName = "Person_Name_Required",ErrorMessageResourceType =typeof(Resource1))]
         public string Name { get; set; }
+        [DisplayName("年齡")]
         [Required(ErrorMessage = "請輸入年紀")]
         [Range(18,99,ErrorMessage ="須為18~99歲")]
         public int Age { get; set; }
